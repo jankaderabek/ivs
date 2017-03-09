003c8ca4c78adfb129aa928b15ba9d4676dca552
ondrejzemanek <ondra.zemanek@gmail.com>
2017-03-09 23:25:53 +0100

    #2 Základní struktura projektu
     - create project structure
     - add project solution

---
+++ src/Calculator/Calculator/ViewModel/Commands/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Calculator.ViewModel.Command
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="execute"></param>
+        public RelayCommand(Action<object> execute)
+            : this(_ => true, execute)
+        {
+        }
+
+        public RelayCommand(Predicate<object> canExecute, Action<object> execute)
+        {
+            this.canExecute = canExecute;
+            this.execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute(parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            var onCanExecuteChanged = this.CanExecuteChanged;
+            if (onCanExecuteChanged != null)
+            {
+                onCanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ src/Calculator/Calculator/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Calculator.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+       
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = this.PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
