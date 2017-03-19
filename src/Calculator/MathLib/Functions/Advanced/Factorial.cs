b12ef07c08e4bb03841246d152a629d5d1ec9a13
Jan Kadeřábek <kaderabek.jan@gmail.com>
2017-03-19 11:16:38 +0100

    #3 MathLibraryTests
     - add factorial method

---
+++ src/Calculator/MathLib/Functions/Advanced/Factorial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathLib.Interfaces;
+
+namespace MathLib.Functions.Advanced
+{
+    public class Factorial : IAdvancedMethod
+    {
+        public double Calculate(double operand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
