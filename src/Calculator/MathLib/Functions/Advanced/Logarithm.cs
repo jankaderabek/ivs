d4af0a264c61d3b212eaac9fc193be8bea0f33fa
Jan Kadeřábek <kaderabek.jan@gmail.com>
2017-03-19 18:29:54 +0100

    #3 MathLibraryTests
     - logarithm method

---
+++ src/Calculator/MathLib/Functions/Advanced/Logarithm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathLib.Interfaces;
+
+namespace MathLib.Functions.Advanced
+{
+    public class Logarithm : IAdvancedMethod
+    {
+        /// <summary>
+        /// Calculate logarithm value with base equal number 10
+        /// </summary>
+        public double Calculate(double operand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Calculator/MathLibraryTests/AdvancedMethods/LogarithmTest.cs
@@ -0,0 +1,35 @@
+﻿using MathLib.Exception;
+using MathLib.Functions.Advanced;
+using MathLib.Functions.Basic;
+using Xunit;
+
+namespace MathLibraryTests.AdvancedMethods
+{
+    public class LogairthmTest
+    {
+        [Fact]
+        public void SimpleCalculateTest()
+        {
+            Logarithm logarithm = new Logarithm();
+
+            Assert.Equal(1, logarithm.Calculate(10));
+        }
+
+        [Fact]
+        public void FloatTest()
+        {
+            Logarithm logarithm = new Logarithm();
+            var result = logarithm.Calculate(9);
+
+            Assert.InRange(result, 0.95, 0.96);
+        }
+
+        [Fact]
+        public void InvalidLogarithmTest()
+        {
+            Logarithm logarithm = new Logarithm();
+
+            Assert.Throws(typeof(InvalidArgumentOfLogarithm), () => logarithm.Calculate(0));
+        }
+    }
+}
